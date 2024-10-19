cd487968acc01aa3c5651431bc33869e52b3f492
Toktobekov Aidar <aidar.omurbekov05@gmail.com>
2024-10-19 14:16:22 +0600

    Добавил фикстуры

--- work-api/fixtures.ts
+++ work-api/fixtures.ts
@@ -1,30 +1,51 @@
 import mongoose from 'mongoose';
 import config from './config';
 import User from './models/User';
+import Post from './models/Post';
 
 const run = async () => {
   await mongoose.connect(config.database);
   const db = mongoose.connection;
 
   try {
+    await db.dropCollection('posts');
     await db.dropCollection('users');
 
   } catch (e) {
     console.log('Skipping drop...');
   }
 
-  await User.create({
+  const [user, admin] = await User.create({
     username: "User",
     password: "123",
     token: crypto.randomUUID(),
-    role: "user"
+    role: "user",
+    displayName: 'User',
   }, {
     username: "Admin",
     password: "321",
     token: crypto.randomUUID(),
-    role: "admin"
+    role: "admin",
+    displayName: 'Admin',
   });
 
+  await Post.create(
+    {
+      username: user.displayName,
+      userId: user._id,
+      image: 'fixtures/image-1',
+      title: 'Горы',
+    },{
+      username: user.displayName,
+      userId: user._id,
+      image: 'fixtures/image-2',
+      title: 'Озеро',
+    },{
+      username: admin.displayName,
+      userId: admin._id,
+      image: 'fixtures/image-3',
+      title: 'Пустыня',
+    });
 
   await db.close();
 };
